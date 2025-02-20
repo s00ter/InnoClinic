f5eeca208198254ccffe322f19a18540abe8642a
s00ter <snodok16@gmail.com>
2025-02-20 18:16:31 +0300

    fix unnecessary transformations

--- AppointmentApi/InnoClinic.AppointmentApi.Api/DependencyInjection/ServiceCollectionExtensions.cs
+++ AppointmentApi/InnoClinic.AppointmentApi.Api/DependencyInjection/ServiceCollectionExtensions.cs
@@ -79,7 +79,7 @@ public static class ServiceCollectionExtensions
                 ValidateAudience = true,
                 ValidAudience = JwtConfiguration.Audience,
                 ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Convert.ToBase64String(SHA256.HashData(Encoding.UTF8.GetBytes(JwtConfiguration.SigningKey))))),
+                IssuerSigningKey = new SymmetricSecurityKey(SHA256.HashData(Encoding.UTF8.GetBytes(JwtConfiguration.SigningKey))),
                 ValidateLifetime = true
             };
         });
