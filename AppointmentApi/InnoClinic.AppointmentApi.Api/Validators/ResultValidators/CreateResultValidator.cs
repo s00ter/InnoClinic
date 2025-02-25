b16cf560a16c848540d19ee924a3c6aa3d61d7c4
s00ter <snodok16@gmail.com>
2025-02-25 18:43:16 +0300

    forgot change

--- AppointmentApi/InnoClinic.AppointmentApi.Api/Validators/ResultValidators/CreateResultValidator.cs
+++ AppointmentApi/InnoClinic.AppointmentApi.Api/Validators/ResultValidators/CreateResultValidator.cs
@@ -8,13 +8,16 @@ public class CreateResultValidator : AbstractValidator<CreateResultRequest>
     public CreateResultValidator()
     {
         RuleFor(x => x.Complaints)
-            .NotEmpty().WithMessage("Complaints is required");
+            .NotEmpty().WithMessage("Complaints is required")
+            .MaximumLength(2048).WithMessage("No more than 2048 characters");;
         
         RuleFor(x => x.Conclusion)
-            .NotEmpty().WithMessage("Conclusion is required");
+            .NotEmpty().WithMessage("Conclusion is required")
+            .MaximumLength(2048).WithMessage("No more than 2048 characters");;
         
         RuleFor(x => x.Recommendations)
-            .NotEmpty().WithMessage("Recommendations is required");
+            .NotEmpty().WithMessage("Recommendations is required")
+            .MaximumLength(2048).WithMessage("No more than 2048 characters");;
         
         RuleFor(x => x.AppointmentId)
             .NotEmpty().WithMessage("AppointmentId is required");
